1502ef14946dafec0f801a50af0d0720cccdb42c
Anirudh <anirudhpatnaik2804@gmail.com>
2024-07-29 17:09:14 +0530

    added authentication logic

--- errors/index.js
+++ errors/index.js
@@ -2,10 +2,12 @@ const BadRequest = require("./bad-request");
 const CustomApiError = require("./custom-api-error");
 const NotFoundError = require("./not-found");
 const UnauthenticatedError = require("./unauthenticated-error");
+const UnauthorizedError = require("./unauthorized");
 
 module.exports = {
     BadRequest,
     CustomApiError,
     NotFoundError,
-    UnauthenticatedError
+    UnauthenticatedError,
+    UnauthorizedError
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,11 +5,15 @@ const {
   updateUser,
   updateUserPassword,
 } = require("../controller/userController");
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/", getAllUser);
+router.get("/", authenticateUser, authorizePermissions('admin'), getAllUser);
 
 router.get("/showCurrentUser", showCurrentUser);
 router.patch("/updateUser", updateUser);
