dbc10a6a2782bc1bedb4b72c08552474cc64f762
Anirudh <anirudhpatnaik2804@gmail.com>
2024-07-30 13:51:30 +0530

    completed: userController logic

--- controller/userController.js
+++ controller/userController.js
@@ -1,35 +1,72 @@
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const User = require("../models/userModel");
+const { createTokenUser, attachCookiesToResponse, checkPermissions } = require("../utils");
 
-const getAllUser = async(req, res) => {
-    const users = await User.find({role: 'user'}).select('-password')
-    res.status(StatusCodes.OK).json({users})
-}
+const getAllUser = async (req, res) => {
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(StatusCodes.OK).json({ users });
+};
 
 const getSingleUser = async (req, res) => {
-    const user = await User.findOne({_id: req.params.id}).select('-password')
-    if(!user){
-        throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`)
-    }
-    res.status(StatusCodes.OK).json({user})}
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`);
+  }
+  checkPermissions(req.user, user._id)
+  res.status(StatusCodes.OK).json({ user });
+};
 
 const showCurrentUser = (req, res) => {
-    res.send("showCurrentUser")
-}
+  res.status(StatusCodes.OK).json({ user: req.user });
+};
 
-const updateUser = (req, res) => {
-    res.send("updateUser")
-}
+const updateUser = async (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email) {
+    throw new CustomError.BadRequestError("Please provide name and email");
+  }
+  const user = await User.findOneAndUpdate(
+    { _id: req.user.userId },
+    { name, email },
+    { new: true, runValidators: true }
+  );
 
-const updateUserPassword = (req, res) => {
-    res.send("updateUserPassword")
-}
+//   const user = await User.findOne({ _id: req.user.userId });
+//   user.email = email
+//   user.name = name
+//   await user.save()
+
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
+
+const updateUserPassword = async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError(
+      "oldPassword, newPassword cannot be empty!"
+    );
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+
+  user.password = newPassword;
+
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: "Success! Password updated." });
+};
 
 module.exports = {
-    getAllUser,
-    getSingleUser,
-    showCurrentUser,
-    updateUser,
-    updateUserPassword
-}
+  getAllUser,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+};
