e53844cab5183d7f114f7e6279e4583ff732b319
Anirudh <anirudhpatnaik2804@gmail.com>
2024-07-30 13:47:17 +0530

    fixed: refactoring

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,11 +1,11 @@
 const { StatusCodes } = require("http-status-codes");
 const CustomApiError = require("./custom-api-error");
 
-class BadRequest extends CustomApiError{
+class BadRequestError extends CustomApiError{
     constructor(message){
         super(message)
         this.StatusCodes = StatusCodes.BAD_REQUEST
     }
 }
 
-module.exports = BadRequest
+module.exports = BadRequestError
