04bbe00935a55200419b229b2b3717ba6bd9b0c7
Anirudh <anirudhpatnaik2804@gmail.com>
2024-07-30 13:53:02 +0530

    fixed: refactoring

--- errors/index.js
+++ errors/index.js
@@ -1,11 +1,11 @@
-const BadRequest = require("./bad-request");
+const BadRequestError = require("./bad-request");
 const CustomApiError = require("./custom-api-error");
 const NotFoundError = require("./not-found");
 const UnauthenticatedError = require("./unauthenticated-error");
 const UnauthorizedError = require("./unauthorized");
 
 module.exports = {
-    BadRequest,
+    BadRequestError,
     CustomApiError,
     NotFoundError,
     UnauthenticatedError,
