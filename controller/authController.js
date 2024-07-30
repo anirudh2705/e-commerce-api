ec1014740dbeb6c9aa101031a96583e1720255db
Anirudh <anirudhpatnaik2804@gmail.com>
2024-07-30 13:48:11 +0530

    fixed: refactoring

--- controller/authController.js
+++ controller/authController.js
@@ -2,14 +2,14 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const User = require("../models/userModel");
 
-const { attachCookiesToResponse } = require("../utils");
+const { attachCookiesToResponse, createTokenUser } = require("../utils");
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
 
   const emailAlreadyexists = await User.findOne({ email });
   if (emailAlreadyexists) {
-    throw new CustomError.BadRequest("Email Already Exixts");
+    throw new CustomError.BadRequestError("Email Already Exixts");
   }
 
   // first user is admin
@@ -17,7 +17,7 @@ const register = async (req, res) => {
   const role = isFirstAccount ? "admin" : "user";
 
   const user = await User.create({ email, name, password, role });
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
@@ -28,19 +28,19 @@ const login = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (!email || !password) {
-    throw new CustomError.BadRequest("Please provide email and password");
+    throw new CustomError.BadRequestError("Please provide email and password");
   }
 
   if (!user) {
-    throw new CustomError.Unauthorized("Invalid Credentials");
+    throw new CustomError.UnauthorizedError("Invalid Credentials");
   }
 
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    throw new CustomError.Unauthorized("Invalid Credentials");
+    throw new CustomError.UnauthorizedError("Invalid Credentials");
   }
 
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
