6d02fd82d3dfe9c722db80382c34c5605eed46d2
Anirudh <anirudhpatnaik2804@gmail.com>
2024-08-01 17:24:03 +0530

    added: review model

---
+++ models/reviewModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please provide rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide title"],
+      maxlength: [100, "Review title cannot exceed 100 characters"],
+    },
+    comment: {
+      type: String,
+      required: [true, "please provide review comment"],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model("Review", ReviewSchema);
